9b2f9ce72a1c9f795d9a23a0fcdf4953d25aa9b5
VishalChaluvadi <vishalchaluvadi007@gmail.com>
2026-01-19 23:38:52 +0530

    Add files via upload

---
+++ madlibs.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char noun[50];
+    char verb[50];
+    char adjective1[50];
+    char adjective2[50];
+    char adjective3[50];
+
+    printf("Enter an adjective (description): ");
+    fgets(adjective1, sizeof(adjective1), stdin);
+    adjective1[strcspn(adjective1, "\n")] = '\0';
+
+    printf("Enter a noun (animal or person): ");
+    fgets(noun, sizeof(noun), stdin);
+    noun[strcspn(noun, "\n")] = '\0';
+
+    printf("Enter another adjective (description): ");
+    fgets(adjective2, sizeof(adjective2), stdin);
+    adjective2[strcspn(adjective2, "\n")] = '\0';
+
+    printf("Enter a verb ending with -ing: ");
+    fgets(verb, sizeof(verb), stdin);
+    verb[strcspn(verb, "\n")] = '\0';
+
+    printf("Enter one more adjective (description): ");
+    fgets(adjective3, sizeof(adjective3), stdin);
+    adjective3[strcspn(adjective3, "\n")] = '\0';
+
+    printf("\n--- Your Mad Lib Story ---\n");
+    printf("Today I went to a %s zoo.\n", adjective1);
+    printf("In an exhibit, I saw a %s.\n", noun);
+    printf("The %s was %s and %s!\n", noun, adjective2, verb);
+    printf("It was %s!\n", adjective3);
+
+    return 0;
+}
+
